ace28c1c79a030ef9f785507034ceede0e2eb469
Enrico Giordano <enrico.giordano@makarenalabs.com>
2023-05-11 01:40:15 +0200

    fixed rom generator script

--- rom-generator/build_roms_pacman.bat
+++ rom-generator/build_roms_pacman.bat
@@ -6,12 +6,12 @@ set rom_path=..\pacman_roms\
 romgen %rom_path%82s126.1m PACROM_1M 9 l r e > %rom_path%pacrom_1m.vhd
 romgen %rom_path%82s126.3m PACROM_1M 9 l r e > %rom_path%pacrom_1m.vhd
 romgen %rom_path%82s126.4a PACROM_4A_DST 8 c > %rom_path%pacrom_4a_dst.vhd
-romgen %rom_path%pacman.5e PACROM_5E 12 c > %rom_path%pacrom_5e.vhd
-romgen %rom_path%pacman.5f PACROM_5F 12 c > %rom_path%pacrom_5f.vhd
-romgen %rom_path%pacman.6e PACROM_6E 12 c > %rom_path%pacrom_6e.vhd
-romgen %rom_path%pacman.6f PACROM_6F 12 c > %rom_path%pacrom_6f.vhd
-romgen %rom_path%pacman.6h PACROM_6H 12 c > %rom_path%pacrom_6h.vhd
-romgen %rom_path%pacman.6j PACROM_6J 12 c > %rom_path%pacrom_6j.vhd
+romgen %rom_path%pacman.5e PACROM_5E 12 c r e > %rom_path%pacrom_5e.vhd
+romgen %rom_path%pacman.5f PACROM_5F 12 c r e > %rom_path%pacrom_5f.vhd
+romgen %rom_path%pacman.6e PACROM_6E 12 c r e > %rom_path%pacrom_6e.vhd
+romgen %rom_path%pacman.6f PACROM_6F 12 c r e > %rom_path%pacrom_6f.vhd
+romgen %rom_path%pacman.6h PACROM_6H 12 c r e > %rom_path%pacrom_6h.vhd
+romgen %rom_path%pacman.6j PACROM_6J 12 c r e > %rom_path%pacrom_6j.vhd
 romgen %rom_path%82s123.7f PACROM_7F_DST 4 c > %rom_path%pacrom_7f_dst.vhd
 
 echo done
